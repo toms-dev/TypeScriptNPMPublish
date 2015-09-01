efe86cadb26f7366df6c5d3fb846aab7b967743e
Tom GUILLERMIN <tom.guillermin@gmail.com>
2015-09-01 15:54:54 +0200

    Split in directories to match TypeScript versions

---
+++ TypeScript-1.5.3/Main.ts
@@ -0,0 +1,19 @@
+/// <reference path="node_modules/my_module_es6/typings.d.ts" />
+
+import myModule = require('my_module_es6');	// this won't work if target ES6
+import myModuleDefault, {MyClass, AnotherClass} from 'my_module_es6';
+
+// From named import
+var obj = new MyClass();
+obj.myMethod("test");
+
+// From require import
+var obj2 = new myModule.AnotherClass();
+obj2.doSomething("hello");
+
+// Default export of the module
+var obj3 = new myModuleDefault();
+obj3.myMethod("test");
+
+console.log("ciao");
+
